116525f5e7f4265ab4f22dc994d4392cf2450b06
ChildishMeteor <35180847+lrdelaportilla@users.noreply.github.com>
2020-02-24 21:20:56 -0500

    Add files via upload

--- starynight.py
+++ starynight.py
@@ -5,7 +5,28 @@ bob = turtle.Turtle()
 screen = turtle.colormode(255)
 root = turtle.Screen()
 root.bgcolor("darkblue")
+bob.color("darkblue")
 bob.speed(0)
+
+#moon
+bob.penup()
+bob.goto(0,100)
+bob.pendown()
+bob.right(90)
+bob.fillcolor("white")
+bob.begin_fill()
+bob.circle(120)
+bob.end_fill()
+bob.fillcolor("darkblue")
+bob.penup()
+bob.goto(-10,100)
+bob.pendown()
+bob.begin_fill()
+bob.circle(100)
+bob.end_fill()
+
+
+#stars
 
 def shape(length,corners,angles,border,fill,x,y):
   bob.penup()
@@ -22,13 +43,12 @@ def shape(length,corners,angles,border,fill,x,y):
   bob.end_fill()
 
 
-#def random_square():
-for i in range(99):
+for i in range(30):
     
     x = random.randint(-300,300)
     y = random.randint(-300,300)
 
-    size = random.randint(10,100)
+    size = random.randint(10,20)
 
     r = random.randint(0,255)
     g = random.randint(0,255)
